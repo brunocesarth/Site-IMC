24812b8826ddb6efb80dfd5df93d539633911405
Bruno Cesar Machado Thobias <bruno.cm.thobias@hotmail.com>
2022-06-23 21:27:55 -0300

    Correção de bug

--- assets/js/main.js
+++ assets/js/main.js
@@ -39,16 +39,16 @@ function verificaResultado(imc) {
     if (imc < 18.5) {
         container.style.cssText = 'background-color: rgb(0, 252, 38)';
         mensagem.innerHTML = `Seu IMC é ${imc}(Abaixo do peso)`;
-    } else if (imc >= 18.5 && imc <= 24.9) {
+    } else if (imc >= 18.5 && imc <= 24.99) {
         container.style.cssText = 'background-color: rgb(0, 252, 38)';
         mensagem.innerHTML = `Seu IMC é ${imc}(Peso normal)`;
-    } else if (imc >= 25 && imc <= 29.9) {
+    } else if (imc >= 25 && imc <= 29.99) {
         container.style.cssText = 'background-color: rgb(0, 252, 38)';
         mensagem.innerHTML = `Seu IMC é ${imc}(Sobrepeso)`;
-    } else if (imc >= 30 && imc <= 34.9) {
+    } else if (imc >= 30 && imc <= 34.99) {
         container.style.cssText = 'background-color: rgb(0, 252, 38)';
         mensagem.innerHTML = `Seu IMC é ${imc}(Obesidade grau 1)`;
-    } else if (imc >= 35 && imc <= 39.9) {
+    } else if (imc >= 35 && imc <= 39.99) {
         container.style.cssText = 'background-color: rgb(0, 252, 38)';
         mensagem.innerHTML = `Seu IMC é ${imc}(Obesidade grau 2)`;
     } else if (imc > 40) {
